3a99ce40409547e832db5c0b0aa21662ad998e5c
barfelmag <bartosz.kopec98@gmail.com>
2022-01-20 00:13:29 +0100

    Pierwsza iteracja modelu bazowego

--- IUM/main.py
+++ IUM/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from ..models import predictPurchase
 
 app = FastAPI()
 
--- models/predictPurchase.py
+++ models/predictPurchase.py
@@ -7,3 +7,9 @@ def predictPurchase(modelInput):
     if os.path.exists('dqn_weights.h5f'):
         model.load_weights('dqn_weights.h5f')
     return model(modelInput)
+
+def basicPredictionModel(modelInput):
+    if modelInput[1] >= 10 and modelInput[1] <= 15:
+        return 1
+    else:
+        return 0
